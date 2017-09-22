4e6d8f29d4aaca8b91f03f00a350ac8a15d4267a
José Tomás González <jtgonzalez@miuandes.cl>
2017-09-22 19:43:33 -0300

    Code Cleaning: Adapted code for new requirements. Updated message parsing and manipulation.

--- main.h
+++ main.h
@@ -11,7 +11,6 @@ void* asyncTransactionBroadcast(void* argunemts);
 void* asyncPostTransaction(void* arguments);
 void* asyncListenTransactions(void* arguments);
 char* intToString(int pid);
-char* useMessage(int* officePID, long long int message, int* accountsArray, char** errorsArray, int* transactionsArray);
 void storeTransacction(int* transactionsArray, int transactionValue);
 void broadcastFromPipe(void* arguments);
 void broadcastDumpCommand(const int bankPID, int* childPID, int* toBankPipe);
@@ -19,7 +18,23 @@ void broadcastDumpAccsCommand(const int bankPID, int* childPID, int* toBankPipe)
 void broadcastDumpErrsCommand(const int bankPID, int* childPID, int* toBankPipe);
 char* generateTransaction(int pidBank, int pidOffice);
 char* messageToString(long long int message);
-int getChildPID(long long int message);
+
+// Message parsing functions
+struct messageData;
+
+int shouldExecuteMessage(int destinationPid, int currentPid);
+
+int parseNumericMessage(struct messageData *parsedMessage, long long int numericMessage);
+int getSourcePid(long long int message);
+int getSourceAccount(long long int message);
+int getDestinationPid(long long int message);
+int getBankPID(long long int message);
+int getDestinationAccount(long long int message);
+int getTransactionAmount(long long int message);
+int getOperationCommand(long long int message);
+
+char* executeMessageOperation(struct messageData* parsedMessage, int* accountsArray, char** errorsArray, int* transactionsArray);
+
 void generateDumpRequest(int pidBank, int pidOffice, int dumpCode, int* toBankPipe);
 char* parseFileName(char* rawFileName, int officePID);
 void logError(char** errorsArray, char* errorMessage);
